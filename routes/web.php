f8755fdc038693087945c868f164521131aa677a
akira <akira.github@gmail.com>
2018-08-06 08:41:32 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 /*
 Route::get('tasks/{id}', 'TasksController@show');
@@ -24,3 +26,7 @@ Route::get('tasks/{id}/edit', 'TasksControlle@edit');
 */
 
 Route::resource('tasks', 'TasksController');
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
